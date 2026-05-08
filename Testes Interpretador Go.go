7fd49e19fd2535e97e303557cedff7ebe99d1fea
Daniel Kobata <kobata.daniel@gmail.com>
2026-05-08 13:26:34 -0300

    Add files via upload

---
+++ Testes Interpretador Go.go
@@ -0,0 +1,52 @@
+Testes Interpretador Go:
+
+1. Print Comum
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+2. Print sem '}'
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+3. Declaração e atribuição de variável
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 10
+	fmt.Println(a)
+}
+
+4. Declaração curta de variável
+package main
+
+import "fmt"
+
+func main() {
+	b := 5
+	fmt.Println(b)
+}
+
+5. Definição de função
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(1, 2))
+}
+
+func soma(x, y int) int {
+	return x + y
+}
